d0794a32c0fc3c845cf97627c889b37b9d071287
lauluDev <LauluDev@brds-MacBook-Pro.local>
2020-12-08 22:56:14 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB1l1jmRARtaykKa3mq7pwUuU2BYV0U6gk",
+  authDomain:  "todolist-c0529.firebaseapp.com",
+  databaseURL: "https://todolist-c0529-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todolist-c0529",
+  storageBucket: "todolist-c0529.appspot.com",
+  messagingSenderId: "716297037185",
+  appId: "1:716297037185:web:71acb300d335a542bc3480",
+  measurementId: "G-FPSBVMBP1L"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  // AngularFireModule.initializeApp(firebaseConfig),
+  // AngularFireDatabaseModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import { AngularFireDatabase } from '@angular/firebase/database';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  
+  constructor(
+    public afDB: AngularFireDatabase,
+  ) {}
 
 }
