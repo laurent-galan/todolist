62f4956db77933c44c5d30f3896f174888e3d8ac
lauluDev <LauluDev@brds-MacBook-Pro.local>
2020-12-08 23:34:36 +0100

    end of year but not successful

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from '@angular/firebase/database';
+import { AngularFireDatabase } from '@angular/fire/database';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currentDate: string;
+  addTask: boolean;
+  tasks = [];
+  myTask = '';
   
   constructor(
     public afDB: AngularFireDatabase,
-  ) {}
+  ) {
+   
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
+
+  addTaskToFirebase() {
+    this.afDB.list('Tasks/').push({
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false
+    });
+    this.showForm();
+  }
+
+  getTasks() {
+    this.afDB.list('Tasks/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.tasks = [];
+      actions.forEach(action => {
+        this.tasks.push({
+          key: action.key,
+          text: action.payload.exportVal().text,
+          hour: action.payload.exportVal().date.substring(11, 16),
+          checked: action.payload.exportVal().checked
+        });
+      });
+    });
+  }
+
+  changeCheckState(ev: any) {
+    console.log('checked: ' + ev.checked);
+    this.afDB.object('Tasks/' + ev.key + '/checked/').set(ev.checked);
+  }
+
+  deleteTask() {
+    this.afDB.list('Tasks/').remove(task.key);
+  }
 
 }
